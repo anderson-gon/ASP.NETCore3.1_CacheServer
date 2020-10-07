9a6a4d9c153f615fa20150e11675aec46ce02aa4
Juliana C g p goncalez <anderson.gon@gmail.com>
2020-10-07 20:57:42 -0300

    add lock

--- src/Infra/CacheServer.Infra/Data/CacheProvider.cs
+++ src/Infra/CacheServer.Infra/Data/CacheProvider.cs
@@ -8,6 +8,7 @@ namespace CacheServer.Infra.Data
     public class CacheProvider
     {        
         private MemoryCache Cache { get; set; }
+        private object _lockObj = new object();
 
         private MemoryCacheEntryOptions GetDefaultEntryOptions()
         {
@@ -27,31 +28,47 @@ namespace CacheServer.Infra.Data
 
         public ValueTask<CacheItem> GetAsync(object key)
         {
-            return new ValueTask<CacheItem>(Cache.Get<CacheItem>(key));
+            lock (_lockObj)
+            {
+                return new ValueTask<CacheItem>(Cache.Get<CacheItem>(key));
+            }
+            
         }
 
         public ValueTask<int> CountObjects()
         {
-            return new ValueTask<int>(Cache.Count);
+            lock (_lockObj)
+            {
+                return new ValueTask<int>(Cache.Count);
+            }
         }
         
         public ValueTask<CacheItem> AddAsync(CacheItem obj)
         {
-            return new ValueTask<CacheItem>(Cache.Set<CacheItem>(obj.Key, obj, GetDefaultEntryOptions()));
+            lock (_lockObj)
+            {
+                return new ValueTask<CacheItem>(Cache.Set<CacheItem>(obj.Key, obj, GetDefaultEntryOptions()));
+            }
         }
         
         public ValueTask RemoveAsync(object key)
         {
             return new ValueTask(Task.Run(() =>
             {
-                Cache.Remove(key);
-                Cache.Compact(.33);
+                lock (_lockObj)
+                {
+                    Cache.Remove(key);
+                    Cache.Compact(.33);
+                }
             }));
         }
 
         public ValueTask<CacheItem> UpdateAsyn(CacheItem obj)
         {
-            return new ValueTask<CacheItem>(Cache.Set<CacheItem>(obj.Key, obj, GetDefaultEntryOptions()));
+            lock (_lockObj)
+            {
+                return new ValueTask<CacheItem>(Cache.Set<CacheItem>(obj.Key, obj, GetDefaultEntryOptions()));
+            }
         }
 
     }
